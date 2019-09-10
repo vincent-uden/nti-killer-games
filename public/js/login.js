00009dc7fb3fe6aba92d78a6231e09d264bc21e0
Vincent Udén <vincentuden@gmail.com>
2019-09-10 22:35:31 +0000

    Small change on font size

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,7 @@ let interval = setInterval(function() {
         if (window.innerWidth > 500) {
             clearInterval(interval);
             textFit(document.getElementById("ntih1"), {widthOnly: true, maxFontSize: 200});
+            textFit(document.getElementById("killerh1"), {widthOnly: true, maxFontSize: 200, detectMultiLine: false});
         } else {
             clearInterval(interval);
             textFit(document.getElementById("ntih1"), {widthOnly: true, maxFontSize: 800});
