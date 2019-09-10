5107c886f7f68ec59b6b17116788bae25386f93a
Vincent Udén <vincentuden@gmail.com>
2019-09-10 22:10:28 +0000

    Fixed some styling issues

---
+++ public/js/login.js
@@ -0,0 +1,14 @@
+let interval = setInterval(function() {
+    if (window.textFit) {
+        if (window.innerWidth > 500) {
+            clearInterval(interval);
+            textFit(document.getElementById("ntih1"), {widthOnly: true, maxFontSize: 200});
+        } else {
+            clearInterval(interval);
+            textFit(document.getElementById("ntih1"), {widthOnly: true, maxFontSize: 800});
+            textFit(document.getElementById("killerh1"), {widthOnly: true, maxFontSize: 800, detectMultiLine: false});
+        }
+    }
+}, 50);
+
+
--- public/js/onLoad.js
+++ public/js/onLoad.js
@@ -1,6 +1,8 @@
-setTimeout(function () {
+function setViewSize() {
         let viewheight = window.visualViewport.height;
         let viewwidth = window.visualViewport.width;
         let viewport = document.querySelector("meta[name=viewport]");
         viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
-    }, 300);
+}
+
+setTimeout(setViewSize, 300);
